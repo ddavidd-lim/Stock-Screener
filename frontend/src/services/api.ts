f3584c27797b954ec0af306e9037afef209766af
David Lim <david@afyaglobal.com>
2026-01-16 15:51:37 -0800

    fix: env name in API_URL

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,5 @@
+// utils/api.js
+const API_BASE_URL = import.meta.env.VITE_API_URL
+ ? import.meta.env.VITE_API_URL
+  : "http://localhost:8000";
+export default API_BASE_URL;
--- frontend/src/utils/api.ts
+++
@@ -1,5 +0,0 @@
-// utils/api.js
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
- ? import.meta.env.VITE_API_BASE_URL
-  : "http://localhost:8000";
-export default API_BASE_URL;
