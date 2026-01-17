659a58e101e3f497e787d9a9972ebfbd13607d4b
David Lim <david@afyaglobal.com>
2026-01-16 16:21:31 -0800

    chore: api url change

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,3 @@
-// utils/api.js
-const API_BASE_URL = import.meta.env.VITE_API_URL
- ? import.meta.env.VITE_API_URL
-  : "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_URL ?? "http://localhost:8000";
+
 export default API_BASE_URL;
